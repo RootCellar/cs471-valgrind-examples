28295b917c2853c42c4b0deca31c208c946a6180
RootCellar <RootCellar9877@gmail.com>
2024-11-09 17:52:32 -0900

    prevent compiler from optimizing away unused variables and space

--- memory_leak_simple/leak.c
+++ memory_leak_simple/leak.c
@@ -1,10 +1,13 @@
 #include <stdlib.h>
 
 int main(int argc, char** argv) {
+
+  volatile int x;
   int* ptr = malloc(5000*sizeof(int));
 
   // To free or not to free?
   if(argc > 1) free(ptr);
+  else x = ptr[0];
 
   exit(0);
 }
